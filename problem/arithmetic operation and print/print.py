2ca18629f369b3d443601c4d4a71dd5b2b772737
eksh7080 <eksh7080@naver.com>
2022-06-24 20:29:20 +0900

    problem solve

---
+++ problem/arithmetic operation and print/print.py
@@ -0,0 +1,76 @@
+# 2557
+# print("Hello World")
+
+# 10718
+# print("강한친구 대한육군")
+# print("강한친구 대한육군")
+
+# 10171
+# print("\    /\\")
+# print(" )  ( ')")
+# print("(  /  )")
+# print(" \(__)|")
+
+# 10172
+# print("|\_/|")
+# print("|q p|   /}")
+# print('( 0 )"""\\')
+# print('|"^"`    |')
+# print("||_/=\\\\__|")
+
+# 1000
+# A,B = map(int, input().split())
+# print(A+B)
+
+# 1001
+# A,B=input().split()
+# A=int(A)
+# B=int(B)
+# print(A-B)
+
+# 10998
+# A,B = map(int, input().split())
+# print(A*B)
+
+# 1008
+# A,B = map(int, input().split())
+# print(A/B)
+
+# 10869
+# A,B = map(int, input().split())
+# print(A+B)
+# print(A-B)
+# print(A*B)
+# print(int(A/B))
+# print(A%B)
+
+# 10926
+# name = input()
+# print(name+"??!")
+
+# 18108
+# B = int(input())
+# print(B-544) #????? 543으로 하니까 정답
+
+# 10430
+# A,B,C = map(int, input().split())
+# print((A+B)%C)
+# print(((A%C)+(B%C))%C)
+# print((A*B)%C)
+# print(((A%C)*(B%C))%C)
+
+# 2588
+# a = int(input())
+# b = input()
+# print(a*int(b[2]))
+# print(a*int(b[1]))
+# print(a*int(b[0]))
+# print(a*int(b))
+
+# 25083
+# print("         ,r\'\"7")
+# print("r`-_   ,\'  ,/")
+# print(" \\. \". L_r\'")
+# print("   `~\\/")
+# print("      |")
+# print("      |")
