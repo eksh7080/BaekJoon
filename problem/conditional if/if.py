759b4ed46b80438619bef565737b36e5fdfbbd65
eksh7080 <eksh7080@naver.com>
2022-06-25 00:07:45 +0900

    if solve

--- problem/conditional if/if.py
+++ problem/conditional if/if.py
@@ -26,3 +26,60 @@
 #     print("1")
 # else:
 #     print("0")
+
+# 14681
+# x=int(input())
+# y=int(input())
+
+# if 0 <= x <= 1000 and 0 <= y <= 1000:
+#     print("1")
+# elif 0 > x and 0 <= y:
+#     print("2")
+# elif 0 > x and 0 > y:
+#     print("3")
+# else:
+#     print("4")
+
+# 2884
+# h,m = map(int, input().split()) 
+
+# if m >= 45:
+#     print(h,m-45)
+# elif h > 0 and m < 45:
+#     h-=1
+#     print(h,m+15)
+# else:
+#     print(23,m+15)
+
+# 2525
+# h,m = map(int, input().split(" "))
+# cook = int(input())
+
+# h += cook // 60
+# m += cook % 60
+
+# if m >= 60:
+#     h+=1
+#     m -= 60
+
+# if h >= 24:
+#     h -= 24
+
+# print(h,m)
+
+# 2480
+# a,b,c = map(int, input().split())
+# same = a == b == c
+# diff1 = a == b
+# diff2 = a == c
+# diff3 = b == c
+# if same:
+#     print(10000+a*1000)
+# elif diff1:
+#     print(1000+a*100)
+# elif diff2:
+#     print(1000+a*100)
+# elif diff3:
+#     print(1000+b*100)
+# else:
+#     print(max(a,b,c)*100)
